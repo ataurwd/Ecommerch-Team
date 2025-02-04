f7fd1f79041afa678f7c2e75f96d2186f60fff03
unknown <alamin>
2025-02-05 00:59:57 +0600

    server basic setup done

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from volunteer-management server ....')
+  })
+  app.listen(port, () => console.log(`server running on port : ${port}`))
